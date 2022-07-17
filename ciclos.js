a39b3ca1512022d7f6f6ca6b23ee6ff8182dc0c3
Eric Dehner <eric.dehner.ga@gmail.com>
2022-07-17 17:44:48 -0300

    creacion portafolio javascript

---
+++ ciclos.js
@@ -0,0 +1,5 @@
+//Complementario
+let numero = parseInt(prompt("Ingrese cuantas veces quiere que se repita la palabra \ndinosaurio"))
+for (i = 0; i < numero; i++) {
+    console.log("veces dinosaurio")
+}
